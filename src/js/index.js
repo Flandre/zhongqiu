0027e504de72b2a32dd50681d4845a0038b43327
Flandre <flantan233@gmail.com>
2016-09-05 17:17:06 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,12 @@ $('section').css({
   'width': $(window).width(),
   'height': $(window).height()
 });
+
+
+function animation_step1(){
+  $(".p1_moon").addClass("show");
+  setTimeout(function(){
+    $(".p1_bg").addClass("show");
+  },200);
+}
+animation_step1()
