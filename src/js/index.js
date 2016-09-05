abd32b42d88c3b1966456bf2eabca6e64934afd0
Flandre <flantan233@gmail.com>
2016-09-05 17:35:28 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ function animation_step1(){
   $(".p1_moon").addClass("show");
   setTimeout(function(){
     $(".p1_bg").addClass("show");
+    setTimeout(function(){
+      $(".p1_benediction").addClass("show")
+    }, 1600)
   },200);
 }
-animation_step1()
+animation_step1();
