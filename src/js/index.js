1a2236f3b653cb36a25d5a7c21eedd6de0ce867b
Flandre <flantan233@gmail.com>
2016-09-06 11:13:59 +0800

    add img

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 /* init page size */
 $('section').css({
-  'width': $(window).width(),
+  'width': $(window).width() < 540 ? $(window).width() : 540,
   'height': $(window).height()
 });
 /* page 1 */
@@ -8,6 +8,16 @@ function animation_step1() {
   $(".p1_moon").addClass("show");
   setTimeout(function () {
     $(".p1_bg").addClass("show");
+    $(".p1_firefly_container").addClass("show");
+    /* firefly plugin */
+    $.firefly({
+      color: '#ffd',
+      minPixel: 3,
+      maxPixel: 6,
+      total: 20,
+      borderRadius: '50%',
+      on: '.p1_firefly_container'
+    });
     setTimeout(function () {
       $(".p1_benediction").addClass("show");
       setTimeout(function () {
@@ -18,15 +28,7 @@ function animation_step1() {
 }
 
 
-/* firefly plugin */
-$.firefly({
-  color: '#ffd',
-  minPixel: 3,
-  maxPixel: 6,
-  total: 20,
-  borderRadius: '50%',
-  on: '.page_1'
-});
+
 
 /* 执行 */
 animation_step1();
